9fcbe2419997de6c2436ad740a71de031fbc77e0
Ernesto Garbarino <ernesto@garba.org>
2019-08-19 16:31:21 +0100

    Bug fixes

--- chp1/start.sh
+++ chp1/start.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 # start.sh
+gcloud config set compute/europe-west2-a
 gcloud container clusters create my-cluster \
-  --num-nodes=3 \
   --issue-client-certificate \
   --enable-basic-auth \
-  --zone=europe-west2-a \
-#  --cluster-version=1.13.7-gke.19
