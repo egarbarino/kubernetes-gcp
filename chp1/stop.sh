f84f8cbb59ec73fe1aad89c1939e3d7cc6dd2668
Ernesto Garbarino <ernesto@garba.org>
2019-08-23 10:30:06 +0100

    Bug fixes

--- chp1/stop.sh
+++ chp1/stop.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # stop.sh
-gcloud container clusters delete my-cluster --quiet --async 
+gcloud config set compute/zone europe-west2-a
+gcloud container clusters delete my-cluster --async --quiet
