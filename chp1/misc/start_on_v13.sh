b27b9caa5bfa25b5fc8c4ceee9e54de30bf26555
Ernesto Garbarino <ernesto@garba.org>
2019-08-27 14:52:35 +0100

    Bug fixes

--- chp1/misc/start_on_v13.sh
+++ chp1/misc/start_on_v13.sh
@@ -3,6 +3,7 @@
 
 # Fetch the latest build of the 1.13 release
 version=$(gcloud container get-server-config | grep 1.13 | head -n 1 | awk '{ print $2 }')
+echo Release: $version
 gcloud container clusters create my-cluster \
   --issue-client-certificate \
   --enable-basic-auth \
